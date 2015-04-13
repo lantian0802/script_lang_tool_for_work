f6de54582a2f4710dae0edbce50ed96cf6d8ade5
jianying.wcj <jianying.wcj@taobao.com>
2015-04-13 16:27:45 +0800

    添加任务执行脚本

---
+++ python/BatchSubmitSpark.py
@@ -0,0 +1,44 @@
+__author__ = 'jianying.wcj'
+#coding=utf-8
+import sys
+try:
+    import json
+except ImportError:
+    import simplejson as json
+import commands
+import time
+
+def generateSparkSubmitCommand(execParam):
+    paramJson = json.loads(execParam)
+    sparkSubmitCommand = "spark-submit --class "+paramJson["class"]+ \
+                                        " --master "+paramJson["master"]+ \
+                                        " --deploy-mode cluster "+ \
+                                        " --executor-memory "+paramJson["executor-memory"]+ \
+                                        " --total-executor-cores "+paramJson["total-executor-cores"]+ \
+                                        " "+paramJson["appParam"]
+    return sparkSubmitCommand
+
+
+def single_submit_spark_job(execParam):
+    submitCommand = generateSparkSubmitCommand(execParam)
+    start = time.time()
+    print(submitCommand)
+    status,result = commands.getstatusoutput(submitCommand)
+    print(str(status)+" "+result)
+    end = time.time()
+    print "JobName:"+json.loads(execParam)["name"]+" cost="+str(end-start)+"ms"
+
+def batch_submit_spark_job(execPlanPath):
+    file = open(execPlanPath)
+    while 1:
+        line = file.readline()
+        if not line:
+            break
+        single_submit_spark_job(line)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("plan file path parameter expected,please rerun the job with the parameter.")
+    else :
+        execPlanPath = sys.argv[1]
+        batch_submit_spark_job(execPlanPath)
